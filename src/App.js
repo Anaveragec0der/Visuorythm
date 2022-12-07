af2248b83d667a276a1504826e05efccd758a982
Priyanshu Vishwakarma <naveragec0der@gmail.com>
2022-12-08 04:27:40 +0530

    added button for dark theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./styles.css";
 import SortingVisualizer from "./SortingVisualizer";
+import { createContext,useState } from "react";
+import { Radio,Icon } from "semantic-ui-react";
+export const ThemeContext=createContext(null);
 export default function App() {
+  const [theme,setTheme]=useState("dark");
+  const toggleTheme=()=>{
+    setTheme((curr)=>(curr==="light"?"dark":"light"));
+  };
   return (
-    <div className="App">
+    <ThemeContext.Provider value={{theme,toggleTheme}}>
+    <div className="App" id={theme}>
       <SortingVisualizer />
+      <div className="radio-toggle">
+      <Icon name="lightbulb outline"/> <Radio toggle onChange={toggleTheme} /> <Icon name="moon outline"/>
+      </div>
     </div>
+    </ThemeContext.Provider>
   );
 }
